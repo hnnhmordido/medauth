4698e9ad4486ed8f09785b2628715813fef1fc60
hnnhmordido <134917740+hnnhmordido@users.noreply.github.com>
2026-08-11 10:16:46 +0930

    Refactor ReactDOM rendering and service worker registration

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import App from "./App.jsx";
+
 import "./styles.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
 if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register(
-      `${import.meta.env.BASE_URL}sw.js`
-    );
-  });
+  window.addEventListener(
+    "load",
+    () => {
+      navigator.serviceWorker.register(
+        `${import.meta.env.BASE_URL}sw.js`
+      );
+    }
+  );
 }
